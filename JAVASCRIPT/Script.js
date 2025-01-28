a8fe6590f52a747d0a48d65dd4e97e73447486f4
emi230-H <emilio.cardenas.PVR@gmail.com>
2025-01-28 13:46:05 -0600

    Add files via upload

---
+++ JAVASCRIPT/Script.js
@@ -0,0 +1,6 @@
+//Obtene elboton por su id que le puse
+const boton=document.getElementById('botoncito')
+//Agregar un evento al boton
+boton.addEventListener('click',function(){
+    alert("Nah mentira, doxeado tu ip: 195.0.3.236");
+});
